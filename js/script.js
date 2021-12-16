b22ad7793c0a1ea6db1968b88eefcce8eca980d2
AndrianiClaudio <Claudioandriani1994@gmail.com>
2021-12-16 15:12:42 +0100

    inizio struttura data. 'what i need.'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
 // console.log('check');
+const app = new Vue({
+    el: '#app',
+    data: {
+        slider_wrapper: {
+            classlist: ['slider-wrapper'],
+            prev: {
+                classlist: ['prev']
+            },
+            next: {
+                classlist: ['next']
+            },
+            fa_i: { //fa = font awesome
+                angle_left: {
+                    family: 'fas',
+                    prefix: 'fa-',
+                    name: 'angle_left'
+                },
+                angle_right: {
+                    family: 'fas',
+                    prefix: 'fa-',
+                    name: 'angle_right'
+                },
+                circle: {
+                    family: 'fas',
+                    prefix: 'fa-',
+                    name: 'circle'
+                }
+            },
+            images: {
+                classlist: ['images'],
+                dir: 'images',
+                src: [
+                    'images1.jpg',
+                    'images2.jpg',
+                    'images3.jpg',
+                    'images4.jpg'
+                ]
+            },
+            nav: {
+                classlist: ['nav']
+            }
+        }
+    }
+});
